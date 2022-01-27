edfb647a411866b2e405fba78638f3b034d4c871
Odeyalo <theprogrammerinfuture@gmail.com>
2022-01-27 19:56:13 +0200

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Location} from '@angular/common';
+
 @Component({
   selector: 'my-app',
-  template: `<div>
-                    <router-outlet></router-outlet>
-               </div>`,
+  template: `
+    <div>
+      <app-navigation *ngIf="!showNavAndFooter()"></app-navigation>
+      <router-outlet></router-outlet>
+      <app-audio-player *ngIf="!showNavAndFooter()" #audioPlayerComponent></app-audio-player>
+    </div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'spotify-fronted';
+
+  constructor(private router: Location) {
+  }
+
+  showNavAndFooter(): boolean {
+    let url = this.router.path();
+    return url == '/login' || url == '/signup' || url.includes('/partners/artist');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,58 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import {RouterModule, Routes} from "@angular/router";
+import {AppComponent} from './app.component';
+import {BaseRouteReuseStrategy, RouterModule, Routes} from "@angular/router";
 import {FormsModule} from "@angular/forms";
-import { HomepageComponent } from './homepage/homepage.component';
+import {HomepageComponent} from './homepage/homepage.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {BasicAuthHttpInterceptorService} from "./login-page/service/basic-auth-http-interceptor.service";
-import { NotFoundComponent } from './not-found/not-found.component';
+import {NotFoundComponent} from './not-found/not-found.component';
 import {LoginPageComponent} from "./login-page/login-page.component";
-import { SearchComponent } from './search/search.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { AudioPlayerComponent } from './audio-player/audio-player.component';
+import {SearchComponent} from './search/search.component';
+import {NavigationComponent} from './navigation/navigation.component';
+import {AudioPlayerComponent} from './audio-player/audio-player.component';
+import {RootComponent} from './root/root.component';
+import {AudioPlayerRouteReuseStrategy} from "./support/AudioPlayerRouteReuseStrategy";
+import {Test} from "./support/Test";
+import {QueueComponent} from './queue/queue.component';
+import {ArtistComponent} from './artist/artist.component';
+import {UploadAlbumComponent} from './upload-album/upload-album.component';
 
+const artistRoutes = [
+  {path: 'upload', component: UploadAlbumComponent},
+  {path: '', component: ArtistComponent, pathMatch: 'full'}
+]
 const routes: Routes = [
   {path: '', component: HomepageComponent},
   {path: 'login', component: LoginPageComponent},
+  {path: 'search', component: SearchComponent},
+  {path: 'search/:query', component: SearchComponent},
+  {path: 'player', component: AudioPlayerComponent, canDeactivate: [Test]},
+  {path: 'queue', component: QueueComponent},
+  {
+    path: 'partners/artist', children: [
+      {path: '', component: ArtistComponent, pathMatch: 'full'},
+      {path: 'upload', component: UploadAlbumComponent}
+    ]
+  },
   {path: '**', component: NotFoundComponent}
+
 ];
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
-    HomepageComponent,
+    [HomepageComponent, AudioPlayerComponent],
     NotFoundComponent,
     SearchComponent,
     NavigationComponent,
-    AudioPlayerComponent
+    AudioPlayerComponent,
+    RootComponent,
+    QueueComponent,
+    ArtistComponent,
+    UploadAlbumComponent,
   ],
   imports: [
     HttpClientModule,
@@ -36,7 +62,11 @@ const routes: Routes = [
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: BasicAuthHttpInterceptorService, multi: true},
+    {provide: BaseRouteReuseStrategy, useClass: AudioPlayerRouteReuseStrategy}
+    // {provide: Test}
+
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
