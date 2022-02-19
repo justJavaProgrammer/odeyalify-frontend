4721eb9972974382b14123b9ad35da4f4bdfc6e5
Odeyalo <theprogrammerinfuture@gmail.com>
2022-02-19 13:20:28 +0200

    Updated

--- src/app/audio-player/audio-player.component.ts
+++ src/app/audio-player/audio-player.component.ts
@@ -1,9 +1,14 @@
-import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
+import {AfterViewInit, Component, HostListener, Inject, OnDestroy, OnInit} from '@angular/core';
 import {Song} from "./Song";
 import {AudioPlayerService} from "./AudioPlayerService";
 import {ActivatedRouteSnapshot, BaseRouteReuseStrategy, DetachedRouteHandle, RouteReuseStrategy} from "@angular/router";
 import {SongDto} from "../dto/SongDTO";
 import {Queue} from "../queue/Queue";
+import {DOCUMENT} from "@angular/common";
+import {AudioPlayerWebSocketSyncHandler} from "./sync/AudioPlayerWebSocketSyncHandler";
+import {AudioPlayerStateDTO} from "./sync/AudioPlayerStateDTO";
+import {Device} from "./sync/Device";
+import {HttpService} from "../http.service";
 
 @Component({
   selector: 'app-audio-player',
@@ -11,18 +16,24 @@ import {Queue} from "../queue/Queue";
   styleUrls: ['./audio-player.component.css']
 })
 export class AudioPlayerComponent implements OnInit, AfterViewInit {
+
   duration?: string;
   currentTime?: string;
   private playButton: any;
   private currentSongId: string = '';
+  private fullScreenElement: any;
+  private _clicked: boolean = false;
+  private _connectedDevices: Device[] = [];
+  devices: any;
 
-  constructor(private audioPlayerService: AudioPlayerService) {
+  constructor(private audioPlayerService: AudioPlayerService,
+              @Inject(DOCUMENT) private document: any, private httpClient: HttpService) {
   }
 
 
   ngOnInit(): void {
     this.audioPlayerService.init();
-    console.log("audio player is created")
+    this.fullScreenElement = document.documentElement;
   }
 
   ngAfterViewInit() {
@@ -32,7 +43,6 @@ export class AudioPlayerComponent implements OnInit, AfterViewInit {
   }
 
   resumeOrPause(): void {
-    console.log(this.audioPlayerService.getIsPlaying())
     if (this.isPlaying()) {
       this.stopSong();
     } else {
@@ -45,9 +55,11 @@ export class AudioPlayerComponent implements OnInit, AfterViewInit {
     this.audioPlayerService.playSong(id);
     this.currentSongId = id;
   }
+
   resumePlaying(): void {
     this.audioPlayerService.resumePlaying();
   }
+
   stopSong() {
     this.audioPlayerService.stopCurrentSong();
   }
@@ -71,11 +83,11 @@ export class AudioPlayerComponent implements OnInit, AfterViewInit {
     return this.audioPlayerService.getCurrentTime();
   }
 
-  playAlbum(albumId:string) {
+  playAlbum(albumId: string) {
     this.audioPlayerService.playAlbum(albumId);
   }
 
-  isPlaying():boolean {
+  isPlaying(): boolean {
     return this.audioPlayerService.getIsPlaying();
   }
 
@@ -86,11 +98,64 @@ export class AudioPlayerComponent implements OnInit, AfterViewInit {
   getArtistId(): string {
     return this.audioPlayerService.getCurrentTrack()?.artistResponseDTO.artistId!;
   }
+
   getArtistName(): string {
     return this.audioPlayerService.getCurrentTrack()?.artistResponseDTO.artistName!;
   }
 
   getTrackName(): string {
     return this.audioPlayerService.getCurrentTrack()?.songName!;
+  }
+
+  isValid(): boolean {
+    return Queue.getCurrentTrack() != undefined;
+  }
+
+  openFullScreenMode() {
+    if (this.fullScreenElement.requestFullscreen) {
+      this.fullScreenElement.requestFullscreen();
+    } else if (this.fullScreenElement.mozRequestFullScreen) {
+      this.fullScreenElement.mozRequestFullScreen();
+    } else if (this.fullScreenElement.webkitRequestFullscreen) {
+      this.fullScreenElement.webkitRequestFullscreen();
+    } else if (this.fullScreenElement.msRequestFullscreen) {
+      this.fullScreenElement.msRequestFullscreen();
+    }
+  }
+
+  showConnectedDevices(): void {
+    this._clicked = !this.clicked;
+    let devices = this.audioPlayerService.getConnectedDevices();
+    if (this._clicked) {
+      console.log(devices.length)
+      this._connectedDevices = devices;
+    }
+  }
+
+  // @HostListener('window:keydown.space', ['$event'])
+  // handleEvent() {
+  //   this.resumeOrPause();
+  // }
+
+  get connectedDevices(): Device[] {
+    if(this._connectedDevices.length <= 1) {
+      return [];
+    }
+    return this._connectedDevices;
+  }
+
+  set clicked(value: boolean) {
+    this._clicked = value;
+  }
+
+  get clicked(): boolean {
+    return this._clicked;
+  }
+
+  switchDevice(id: string): void {
+   let device = this.audioPlayerService.getCurrentActiveDevice();
+   if(id != device?.deviceId) {
+     this.audioPlayerService.switchDevice(id);
+   }
   }
 }
